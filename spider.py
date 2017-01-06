52ee36b29a2cb7ab74bd08e5956b176040abf11b
Lukey <lshort2@mail.csuchico.edu>
2017-01-06 12:11:42 -0800

    Added 2nd static method to Spider class. Need to Add links to queue

--- spider.py
+++ spider.py
@@ -32,10 +32,21 @@ class Spider:
         self.boot()
         self.crawl_page('First spider', Spider.base_url)
 
-    @staticmethod   #This is a little indicator to python to say, hey this is a static method,
+    @staticmethod   #This is a little indicator to python to say, hey this is a static method, (make PEP8 happy)
     def boot():     #  which also means we do not need to pass self in.
         create_project_dir(Spider.project_name)#create directory
         create_data_files(Spider.project_name, Spider.base_url)#create the .txt files
         Spider.queue =file_to_set(Spider.queue_file)
         Spider.crawled =file_to_set(Spider.crawled_file)
+
+    @staticmethod
+    def crawl_page(thread_name, page_url):
+        if page_url not in Spider.crawled:
+            print(thread_name +' crawling ' +page_url)
+            print( 'Queue ' + str(len(Spider.queue)) +' | Crawled ' +str(len(Spider.crawled)) )
+            Spider.add_links_to_queue(Spider.gather_link(page_url))
+            Spider.queue.remove(page_url)
+            Spider.crawled.add(page_url)
+            Spider.update_files()#after we have done our fast operations w/ our variables, now we can update files on HDD
+
 
